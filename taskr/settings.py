64dcdde1fa04eb735256f072352a1b17983541a6
Anvesh <anvesh.kumar08@gmail.com>
2015-11-16 16:13:35 +0530

    created tasks app, configured mysql

--- taskr/settings.py
+++ taskr/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'taskr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'taskrdb',
+        'USER': 'anvesh',
+        'PASSWORD': 'd3fault',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
