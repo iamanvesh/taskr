454858d6d7b0fba9814df27fd194d2ca23b4cd6c
Anvesh <anvesh.kumar08@gmail.com>
2015-11-16 16:30:53 +0530

    added admin classes to all models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Entry, Keyword, Result
+
+
+class EntryAdmin(admin.ModelAdmin):
+    pass
+
+
+class KeywordAdmin(admin.ModelAdmin):
+    pass
+
+
+class ResultAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(Keyword, KeywordAdmin)
+admin.site.register(Result, ResultAdmin)
