230b558da32ec35ae0125d93c5f0129245cb31e0
Anvesh <anvesh.kumar08@gmail.com>
2015-11-16 16:22:47 +0530

    Added models to tasks app

---
+++ tasks/migrations/0001_initial.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Entry',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('url', models.TextField()),
+                ('email', models.EmailField(max_length=254)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Keyword',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(max_length=50)),
+                ('entry', models.ForeignKey(to='tasks.Entry')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Result',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('isAvailable', models.BooleanField(default=False)),
+                ('keyword', models.ForeignKey(to='tasks.Keyword')),
+            ],
+        ),
+    ]
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.Model):
+    url = models.TextField()
+    email = models.EmailField()
+
+    def __unicode__(self):
+        return self.url
+
+
+class Keyword(models.Model):
+    key = models.CharField(max_length=50)
+    entry = models.ForeignKey(Entry)
+
+    def __unicode__(self):
+        return self.key
+
+
+class Result(models.Model):
+    isAvailable = models.BooleanField(default=False)
+    keyword = models.ForeignKey(Keyword)
+
+    def __unicode__(self):
+        return self.keyword + self.isAvailable
